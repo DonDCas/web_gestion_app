f88f17cd20b22d28c372923308e75ca913499eae
DonDCas <fjrueda92@gmail.com>
2025-12-10 02:00:55 +0100

    Servicio-Mal

--- lib/presentation/screens/monumentos_screen.dart
+++ lib/presentation/screens/monumentos_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:web_gestion_app/models/monumento_config_model.dart';
 import 'package:web_gestion_app/services/monumentos_config_service.dart';
 
 class MonumentosScreen extends StatefulWidget {
@@ -9,9 +10,80 @@ class MonumentosScreen extends StatefulWidget {
 }
 
 class _MonumentosScreenState extends State<MonumentosScreen> {
+  final MonumentosConfigService configService = MonumentosConfigService();
+
   @override
   Widget build(BuildContext context) {
-    MonumentosConfigService config = MonumentosConfigService();
-    return const Placeholder();
+    return FutureBuilder<List<MonumentoConfig>>(
+      future: configService.getMonumentosConfig(),
+      builder: (BuildContext context, AsyncSnapshot<List<MonumentoConfig>> snapshot) {
+        if (!snapshot.hasData) {
+          return const Center(child: CircularProgressIndicator());
+        }
+
+        final monumentos = snapshot.data!;
+
+        return Scaffold(
+          appBar: AppBar(title: const Text('Monumentos')),
+          body: Column(
+            children: [
+              Expanded(
+                child: ListView.builder(
+                  itemCount: monumentos.length,
+                  itemBuilder: (context, index) {
+                    return _MonumentCard(monumento: monumentos[index]);
+                  },
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: IconButton(
+                  onPressed: () {
+                    // Aquí puedes implementar la función de guardar cambios
+                    print('Guardar cambios');
+                  },
+                  icon: const Icon(Icons.save),
+                  tooltip: 'Guardar cambios',
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
+
+class _MonumentCard extends StatefulWidget {
+  final MonumentoConfig monumento;
+  const _MonumentCard({required this.monumento, super.key});
+
+  @override
+  State<_MonumentCard> createState() => _MonumentCardState();
+}
+
+class _MonumentCardState extends State<_MonumentCard> {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(widget.monumento.nombre, style: const TextStyle(fontSize: 16)),
+            Switch(
+              value: widget.monumento.activo,
+              onChanged: (bool value) {
+                setState(() {
+                  widget.monumento.activo = value;
+                });
+              },
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
--- lib/services/monumentos_config_service.dart
+++ lib/services/monumentos_config_service.dart
@@ -1,26 +1,41 @@
 import 'dart:convert';
-
 import 'package:web_gestion_app/models/monumento_config_model.dart';
 import 'package:http/http.dart';
 
-class MonumentosConfigService{
-  final String _endPoint ="https://ejcalientegrupos2025-default-rtdb.europe-west1.firebasedatabase.app/gestion/monumentos.json";
-  Future<List<MonumentoConfig>> getMonumentosConfig() async {
-    List<MonumentoConfig> result = [];
+class MonumentosConfigService {
+  final String _endPoint = "https://ejcalientegrupos2025-default-rtdb.europe-west1.firebasedatabase.app/gestion/monumentos.json";
+Future<List<MonumentoConfig>> getMonumentosConfig() async {
+  List<MonumentoConfig> result = [];
 
+  try {
     Uri uri = Uri.parse(_endPoint);
     Response response = await get(uri);
+
     if (response.statusCode != 200) return [];
 
     final data = jsonDecode(response.body);
 
-    data.forEach((id, objeto){
-      MonumentoConfig temp = MonumentoConfig.fromJson(objeto);
-      temp.id = id;
-      result.add(temp);
-      print(temp);
-    });
+    if (data is Map<String, dynamic>) {
+      // Caso: JSON con IDs como keys
+      data.forEach((id, objeto) {
+        MonumentoConfig temp = MonumentoConfig.fromJson(objeto);
+        temp.id = id;
+        result.add(temp);
+      });
+    } else if (data is List) {
+      // Caso: JSON es un array
+      for (var objeto in data) {
+        MonumentoConfig temp = MonumentoConfig.fromJson(objeto);
+        result.add(temp);
+      }
+    }
 
-    return result;
+  } catch (e) {
+    print('Error al parsear JSON: $e');
+    return [];
   }
+
+  return result;
+}
+
 }
